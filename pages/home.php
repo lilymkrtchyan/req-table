2bd691b609dddba425eb411932eb6e7ca6e57fee
Lili Mkrtchyan <lm688@cornell.edu>
2023-03-20 16:30:32 -0400

    Changed the title

--- pages/home.php
+++ pages/home.php
@@ -115,7 +115,7 @@ const TYPE = array(
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Info Sci Courses</title>
+  <title>Info Sci Courses Catalog</title>
 
   <link rel="stylesheet" type="text/css" href="/public/styles/site.css" media="all">
 </head>
